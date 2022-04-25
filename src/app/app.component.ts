edcbe0ca0f2c062cfa88b5d10fa07e570d46d9c5
Melchior Crinier <66031896+mlchior@users.noreply.github.com>
2022-04-25 17:05:04 +0200

    models - composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FaceSnap} from "./models/face-snaps.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  mySnap2!: FaceSnap;
+  mySnap3!: FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+    "Face Snap",
+    "Face Snap is a web application that allows users to take pictures of faces and save them to a database.",
+    new Date(),
+    0,
+    'https://source.unsplash.com/user/c_v_r'
+    );
+    this.mySnap2 = new FaceSnap(
+      "blah",
+      "lore ipsum10",
+      new Date(),
+      4,
+      'https://source.unsplash.com/user/c_v_r'
+    );
+    this.mySnap3 = new FaceSnap(
+      "blah blah",
+      "lore ipsum12",
+      new Date(),
+      38,
+      'https://source.unsplash.com/user/c_v_r'
+    );
+  }
+
 
 }
---
+++ src/app/models/face-snaps.model.ts
@@ -0,0 +1,12 @@
+export class FaceSnap {
+
+
+  constructor(public title: string,
+              public description: string,
+              public createDate:Date,
+              public snaps:number,
+              public imageUrl:string) {
+
+  }
+
+}
