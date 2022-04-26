540efded0c3b0614b5774a53ef329e120859a0ee
Melchior Crinier <66031896+mlchior@users.noreply.github.com>
2022-04-26 11:45:43 +0200

    ngStyle dynamique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,34 @@ import {FaceSnap} from "./models/face-snaps.model";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  mySnap2!: FaceSnap;
-  mySnap3!: FaceSnap;
+  faceSnaps!: FaceSnap[];
+
 
   ngOnInit() {
-    this.mySnap = {
+    this.faceSnaps = [
+      {
       title: "Face Snap",
       description: "Face Snap is a web application that allows users to take pictures of faces and save them to a database.",
       createDate: new Date(),
-      snaps: 0,
+      snaps: 300,
       imageUrl: 'https://source.unsplash.com/user/c_v_r'
-    };
-    this.mySnap2 = {
+    },
+     {
       title: "blah",
       description: "lorem ipsum10",
       createDate: new Date(),
       snaps: 0,
       imageUrl: 'https://source.unsplash.com/user/c_v_r'
-    };
-    this.mySnap3 = {
+    },
+    {
       title:"blah blah",
       description:"lore ipsum12",
       createDate:new Date(),
       snaps:30  ,
       imageUrl:'https://source.unsplash.com/user/c_v_r',
       location: "paris",
-    };
+    }
+    ];
 
   }
 
